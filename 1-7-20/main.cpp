fe8fe8db3dc1c79470ded607022ffbe6b909b4c4
Jacob Christensen <jacob.h.christensen@gmail.com>
2020-01-09 10:38:26 -0700

    File notes from 1-7

--- 1-7-20/main.cpp
+++ 1-7-20/main.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+void aFunction(int stuff)
+{
+  cout << "Received " << stuff << endl;
+}
+
 int main(void)
 {
-  cout << "Hello World!" << endl;
+  cout << "Hello Tom!" << endl;
+  
+  for (int i = 0; i < 10; ++i)
+  {
+    cout << i << endl;
+  }
+  
+  int input = 0;
+  while (input != 10)
+  {
+    cout << "Enter a number: ";
+    cin >> input;
+  }
+
+  aFunction(29);
+
   return 0;
 }
