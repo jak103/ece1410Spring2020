1394392501db2db29aceb3d3834be71dc10087fa
Jacob Christensen <jacob.h.christensen@gmail.com>
2020-01-21 10:26:25 -0700

    More notes

--- 1-16-20/main.cpp
+++ 1-16-20/main.cpp
@@ -17,23 +17,29 @@ int main()
   cout << "Bob's number: " << bob.getPhoneNumber("Melinda") << endl;
 
   // Parsing 4/2 + 4/1
-  float num1;
-  int den1;
-  char operand;
-  int num2;
-  int den2;
-  char slash;
-  cout << "Input a fraction equation: ";
-  cin >> num1 >> slash >> den1 >> operand >> num2 >> slash >> den2;
+  // float num1;
+  // int den1;
+  // char operand;
+  // int num2;
+  // int den2;
+  // char slash;
+  // cout << "Input a fraction equation: ";
+  // cin >> num1 >> slash >> den1 >> operand >> num2 >> slash >> den2;
 
-  cout << "Num1: " << num1 << endl;
-  cout << "Den1: " << den1 << endl;
-  cout << "Operand: " << operand << endl;
-  cout << "Num2: " << num2 << endl;
-  cout << "Den2: " << den2 << endl;
+  // cout << "Num1: " << num1 << endl;
+  // cout << "Den1: " << den1 << endl;
+  // cout << "Operand: " << operand << endl;
+  // cout << "Num2: " << num2 << endl;
+  // cout << "Den2: " << den2 << endl;
   
+  int a = 7;
+  int b = 9;
 
-  
+  cout << "The address of a: " << &a << endl;
+  cout << "The address of b: " << &b << endl;
+  cout << "size of int: " << sizeof(int) << endl;
+  cout << "string sizeof: " << sizeof(string) << endl;
+  cout << "size of person: " << sizeof(Person) << endl;
 
   return 0;
 }
