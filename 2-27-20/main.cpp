1b5f9e59d0d55760c9b7b121538b95fa60fed159
Jacob Christensen <jacob.h.christensen@gmail.com>
2020-02-27 11:32:11 -0700

    Bad stack behavior

--- 2-27-20/main.cpp
+++ 2-27-20/main.cpp
@@ -10,19 +10,23 @@ int main()
   cout << "Hello memory!" << endl;
 
   int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
+  int temp = 999;
   for (int i = 0; i < 10; ++i)
   {
     cout << "array[" << i << "]: " << array[i] << endl;
   }
+
+  array[11] = 0; // DONT DO THIS!!!
+
+  cout << "temp: " << temp << endl;
 
   cout << "array: " << array << endl;
   cout << "*array: " << *(array + 1) << endl;
 
   int* num = new int;
   *num = 42;
-  int* ptr = new int[10];
-  for (int i = 0; i < 10; ++i)
+  int* ptr = new int; // DONT DO THIS!!!
+  for (int i = 0; i < 10; ++i) // DONT DO THIS!!!
   {
     *(ptr + i) = i;
     cout << "*(ptr + " << i << "): " << ptr[i] << endl;
