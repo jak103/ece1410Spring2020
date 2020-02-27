3b7457b4ec5c8a5eda70eb33414b792d63088738
Jacob Christensen <jacob.h.christensen@gmail.com>
2020-02-27 11:50:20 -0700

    Final notes

--- 2-27-20/main.cpp
+++ 2-27-20/main.cpp
@@ -8,7 +8,8 @@ using namespace std;
 int main()
 {
   cout << "Hello memory!" << endl;
-
+  std::string temp;
+  
   int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   int temp = 999;
   for (int i = 0; i < 10; ++i)
